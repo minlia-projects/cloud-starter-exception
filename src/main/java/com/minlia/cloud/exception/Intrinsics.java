68029ec3f471c3394e7f6e3243ea16b4c0c03032
will <will@minlia.com>
2018-04-10 17:30:34 +0800

    Update

--- src/main/java/com/minlia/cloud/exception/Intrinsics.java
+++ src/main/java/com/minlia/cloud/exception/Intrinsics.java
@@ -1,6 +1,7 @@
 package com.minlia.cloud.exception;
 
 import com.minlia.cloud.context.EnvironmentHolder;
+import org.springframework.http.HttpStatus;
 
 /**
  * @author will
@@ -18,9 +19,24 @@ public class Intrinsics {
     }
   }
 
+  public static void isNull(Object parameter, Object code,HttpStatus status, Object... arguments) {
+    if (null == parameter) {
+      throwExceptionWithCodeAndArguments(code,status, arguments);
+    } else {
+      throwException("[DevelopmentPhaseError]传入的参数${parameter}不能为空");
+    }
+  }
+
   public static void isNotNull(Object parameter, Object code, Object... arguments) {
     if (null != parameter) {
       throwExceptionWithCodeAndArguments(code, arguments);
+    } else {
+      throwException("[DevelopmentPhaseError]传入的参数${parameter}不能为空");
+    }
+  }
+  public static void isNotNull(Object parameter, Object code,HttpStatus status, Object... arguments) {
+    if (null != parameter) {
+      throwExceptionWithCodeAndArguments(code,status, arguments);
     } else {
       throwException("[DevelopmentPhaseError]传入的参数${parameter}不能为空");
     }
@@ -30,10 +46,15 @@ public class Intrinsics {
    * 从系统环境上下文中取出表达式的值${spring.name}，传入 "spring.name" ，检查表达式的值是否为空，为空则抛出异常
    */
   public static void isNull(String parameter, Object code, Object... arguments) {
-//    isNull(parameter,code,arguments);
     String value = EnvironmentHolder.getEnvironment().getProperty(parameter.toString());
     if (null == value) {
       isNull(parameter, code, arguments);
+    }
+  }
+  public static void isNull(String parameter, Object code,HttpStatus status, Object... arguments) {
+    String value = EnvironmentHolder.getEnvironment().getProperty(parameter.toString());
+    if (null == value) {
+      isNull(parameter, code, status,arguments);
     }
   }
 
@@ -42,10 +63,15 @@ public class Intrinsics {
    * 从系统环境上下文中取出表达式的值${spring.name}，传入 "spring.name" ，检查表达式的值是否为空，为空则抛出异常
    */
   public static void isNotNull(String parameter, Object code, Object... arguments) {
-//    isNull(parameter,code,arguments);
     String value = EnvironmentHolder.getEnvironment().getProperty(parameter.toString());
     if (null != value) {
       isNotNull(parameter, code, arguments);
+    }
+  }
+  public static void isNotNull(String parameter, Object code,HttpStatus status, Object... arguments) {
+    String value = EnvironmentHolder.getEnvironment().getProperty(parameter.toString());
+    if (null != value) {
+      isNotNull(parameter, code, status,arguments);
     }
   }
 
@@ -57,6 +83,11 @@ public class Intrinsics {
       throwExceptionWithCodeAndArguments(code, arguments);
     }
   }
+  public static void is(Boolean expression, Object code,HttpStatus status, Object... arguments) {
+    if (expression) {
+      throwExceptionWithCodeAndArguments(code, status,arguments);
+    }
+  }
 
   /**
    * 表达式是否为假，当为假时抛出异常
@@ -65,8 +96,16 @@ public class Intrinsics {
     is(!expression, code, arguments);
   }
 
+  public static void not(Boolean expression, Object code,HttpStatus status, Object... arguments) {
+    is(!expression, code,status, arguments);
+  }
 
-  private static void throwExceptionWithCodeAndArguments(Object code, Object... arguments) {
+
+  private static void throwExceptionWithCodeAndArguments(Object code,  Object... arguments) {
+    throwExceptionWithCodeAndArguments(code,HttpStatus.OK,arguments);
+  }
+
+  private static void throwExceptionWithCodeAndArguments(Object code,HttpStatus status, Object... arguments) {
     //当code为Integer时才可能有参数
     if (null == arguments) {
       //为防止没有传入时出现异常
@@ -76,7 +115,7 @@ public class Intrinsics {
     if (code instanceof String) {
       throw new ApiException(code.toString());
     } else if (code instanceof Integer) {
-      throwException(Integer.valueOf(code.toString()), arguments);
+      throwException(Integer.valueOf(code.toString()),status.value(), arguments);
     } else {
       throwException("[DevelopmentPhaseError]不可以传入非字符串或整型的code参数");
     }
@@ -111,25 +150,6 @@ public class Intrinsics {
     throw new ApiException(code);
   }
 
-
-
-
-//  /**
-//   * 抛出异常
-//   */
-//  public static void throwAuthenticationException(Integer code, Object... arguments) {
-//    throw new AuthenticationException(code, arguments);
-//  }
-//
-//  /**
-//   * 抛出带错误状态的异常
-//   * @param code
-//   * @param httpStatus
-//   * @param arguments
-//   */
-//  public static void throwAuthenticationException(Integer code,Integer httpStatus, Object... arguments) {
-//    throw new AuthenticationException(code,httpStatus, arguments);
-//  }
 
 
 
